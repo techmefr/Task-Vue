b5b32feb1fda2e7c2ed56b4e2d8016104117f030
techmefr <compigni.gaetan@gmail.com>
2025-03-05 14:14:54 +0100

    feat(ui): implement basic todo list functionality

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+    <todo-app />
+  </div>
+</template>
+
 <script>
 import TodoApp from './components/TodoApp.vue'
 
@@ -8,11 +14,5 @@ export default {
   }
 }
 </script>
-
-<template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-    <todo-app />
-  </div>
-</template>
 
 <style></style>
